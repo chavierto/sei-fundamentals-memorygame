66ee555c1701b73418728e9594963afd9cb475c6
Xavier Lujan <hola@xavierlujan.com>
2020-04-08 00:23:44 -0400

    Created game logic. Added arrays,if statements and alert.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
-console.log("Up and running!")
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+console.log("Up and running!");
 
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2)
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
