baf4527c6e12a31c857579963b9b4a21696a1601
Xavier Lujan <hola@xavierlujan.com>
2020-04-08 21:21:56 -0400

    Updated logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,29 @@ console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+function checkForMatch() {
+  if (cardsInPlay.length === 2)
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
+}
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+function flipCard(cardId) {
+/*Older version
+  let cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  console.log("User flipped " + cardOne);
 
-if (cardsInPlay.length === 2)
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+  let cardTwo = cards[2];
+  cardsInPlay.push(cardTwo);
+  console.log("User flipped " + cardTwo);
+*/
+
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
   }
+flipCard(0);
+flipCard(2);
