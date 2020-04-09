bc822204b6d8ce0ede4401898d67eb0796aa18ff
Xavier Lujan <hola@xavierlujan.com>
2020-04-09 04:58:02 -0400

    Added the cards through JS, added dynamic card flipping, added a try again button, finished excercise except for bonus #2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,18 @@
 console.log("Up and running!");
 
+function createBoard() {
+  for (let i=0; i<=3; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/back.png');
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+
+
 const cards = [
   {
     rank: "queen",
@@ -27,29 +40,21 @@ const cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay.length === 2)
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+      alert("You found a match!");
     } else {
       alert("Sorry, try again.");
     }
 }
 
-function flipCard(cardId) {
-/*Older version
-  let cardOne = cards[0];
-  cardsInPlay.push(cardOne);
-  console.log("User flipped " + cardOne);
-
-  let cardTwo = cards[2];
-  cardsInPlay.push(cardTwo);
-  console.log("User flipped " + cardTwo);
-*/
-
+function flipCard() {
+  var cardId = this.getAttribute('data-id')
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId]).rank;
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+  this.setAttribute('src',cards[cardId].cardImage);
   checkForMatch();
   }
-flipCard(0);
-flipCard(2);
+
+createBoard();
